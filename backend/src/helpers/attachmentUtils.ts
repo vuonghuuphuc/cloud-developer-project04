ca99d29f161f006353e7c24b2bdaee26f2d16d1c
Phuc VH <vuonghuuphuc@gmail.com>
2023-03-30 23:05:05 +0700

    update data layer

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -1,6 +1,43 @@
-//import * as AWS from 'aws-sdk'
-//import * as AWSXRay from 'aws-xray-sdk'
+import * as AWS from 'aws-sdk'
 
-//const XAWS = AWSXRay.captureAWS(AWS)
+const AWSXRay = require('aws-xray-sdk')
+const XAWS = AWSXRay.captureAWS(AWS)
 
 // TODO: Implement the fileStogare logic
+export class AttachmentUtils {
+
+    constructor(
+        private readonly s3Client = new XAWS.S3({
+            signatureVersion: 'v4'
+        }),
+        private readonly todosBucket = process.env.ATTACHMENT_S3_BUCKET,
+        private readonly attachmentSignedUrlExpiration = process.env.SIGNED_URL_EXPIRATION,
+        private readonly docClient = new XAWS.DynamoDB.DocumentClient(),
+        private readonly todosTable = process.env.TODOS_TABLE
+    ) {
+    }
+
+    async updateTodoAttachmentUrl(todoId: string, userId: string, attachmentUrl: string){
+        return await this.docClient.update({
+            TableName: this.todosTable,
+            Key: {
+              userId,
+              todoId
+            },
+            UpdateExpression: "set attachmentUrl=:attachmentUrl",
+            ExpressionAttributeValues:{
+                ":attachmentUrl": attachmentUrl,
+            },
+            ReturnValues:"UPDATED_NEW"
+        }).promise()
+    }
+
+    generateAttachmentPresignedUrl(attachmentId: string) {
+        return this.s3Client.getSignedUrl('putObject', {
+            Bucket: this.todosBucket,
+            Key: attachmentId,
+            Expires: parseInt(this.attachmentSignedUrlExpiration)
+        })
+    }
+
+}
--- backend/src/helpers/todosAcess.ts
+++ backend/src/helpers/todosAcess.ts
@@ -1,12 +1,87 @@
-// import * as AWS from 'aws-sdk'
-// import * as AWSXRay from 'aws-xray-sdk'
-// import { DocumentClient } from 'aws-sdk/clients/dynamodb'
-// import { createLogger } from '../utils/logger'
-// import { TodoItem } from '../models/TodoItem'
-// import { TodoUpdate } from '../models/TodoUpdate';
+import * as AWS from 'aws-sdk'
+import { TodoItem } from '../models/TodoItem'
+import { TodoUpdate } from '../models/TodoUpdate';
 
-// const XAWS = AWSXRay.captureAWS(AWS)
-
-// const logger = createLogger('TodosAccess')
+const AWSXRay = require('aws-xray-sdk')
+const XAWS = AWSXRay.captureAWS(AWS)
 
 // TODO: Implement the dataLayer logic
+export class TodosAccess {
+
+    constructor(
+        private readonly docClient = new XAWS.DynamoDB.DocumentClient(),
+        private readonly todosTable = process.env.TODOS_TABLE
+    ) {
+    }
+
+    async createTodo(todoItem: TodoItem): Promise<TodoItem> {
+
+        await this.docClient.put({
+            TableName: this.todosTable,
+            Item: todoItem
+        }).promise()
+
+        return todoItem
+    }
+
+    async getTodos(userId: string): Promise<TodoItem[]> {
+
+        const result = await this.docClient.query({
+            TableName: this.todosTable,
+            KeyConditionExpression: 'userId = :userId',
+            ExpressionAttributeValues: {
+                ':userId': userId
+            }
+        }).promise()
+        return result.Items as TodoItem[]
+    }
+
+    async deleteTodo(todoId: String, userId: String) {
+
+        return await this.docClient.delete({
+            TableName: this.todosTable,
+            Key: {
+                todoId: todoId,
+                userId: userId
+            }
+        }).promise()
+    }
+
+    async updateTodo(todoId: String, userId: String, data: TodoUpdate): Promise<TodoUpdate> {
+
+        await this.docClient.update({
+            TableName: this.todosTable,
+            Key: {
+                userId,
+                todoId
+            },
+            UpdateExpression: "set #name = :name, dueDate=:dueDate, done=:done",
+            ExpressionAttributeValues: {
+                ":name": data.name,
+                ":dueDate": data.dueDate,
+                ":done": data.done
+            },
+            ExpressionAttributeNames: {
+                "#name": "name"
+            },
+            ReturnValues: "UPDATED_NEW"
+        }).promise()
+
+        return data;
+    }
+
+    async checkExists(todoId: String, userId: String) {
+        const result = await this.docClient
+            .get({
+                TableName: this.todosTable,
+                Key: {
+                    todoId: todoId,
+                    userId: userId
+                }
+            })
+            .promise()
+        return !!result.Item
+    }
+
+}
+
